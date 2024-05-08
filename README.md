cc459f05e9c01bd8cd852df4417799a99227302e
Alex Eski <alex.eski@tapcheck.com>
2024-05-08 15:26:48 -0700

    update readme.md and cleanup init.lua

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Neovim Config for C#, Go, Rust, Zig, C, C++, etc. already set up with LSP and Treesitter. Based on https://github.com/nvim-lua/kickstart.nvim.
+# neovim-config
+Setup primarily for C# and Lua development. Based on https://github.com/nvim-lua/kickstart.nvim.
 
-I am working on further refinement, I want to use a tool to interact with git and replace the file tree I have, set up undo tree, refactor my config to clean it up. I also want to look into if I can connect to Rider's LSP, because I really don't like the recommendations from Omnisharp for C# development.
+### Todos
+Add support for Rust, Go, C, C++, and Zig.
+Setup undotree.
+Look into setting up fugitive.
+Setup harpoon.
--- init.lua
+++ init.lua
@@ -1,22 +1,3 @@
---[[
-Neovim configuration based on Kickstart.
-  - lua basics: https://learnxinyminutes.com/docs/lua/
-
-  After understanding a bit more about Lua, you can use `:help lua-guide` as a
-  reference for how Neovim integrates Lua.
-  - :help lua-guide
-
-  If new to vim, run :Tutor.
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Can always run AND READ `:help`.
-
-  MOST IMPORTANTLY, use "<space>sh" to [s]earch the [h]elp documentation.
-]]
---
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -690,49 +671,19 @@ require("lazy").setup({
 		end,
 	},
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	{
 		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			--vim.cmd.hi("Comment gui=none")
 		end,
-		-- opts = {
-		-- 	transparent = true,
-		-- 	styles = {
-		-- 		sidebars = "transparent",
-		-- 		floats = "transparent",
-		-- 	},
-		-- },
 	},
 
-	{ "EdenEast/nightfox.nvim" },
-
-	-- {
-	-- 	"scottmckendry/cyberdream.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("cyberdream").setup({
-	-- 			-- Recommended - see "Configuring" below for more config options
-	-- 			transparent = true,
-	-- 			italic_comments = true,
-	-- 			hide_fillchars = true,
-	-- 			borderless_telescope = true,
-	-- 			terminal_colors = true,
-	-- 		})
-	-- 		vim.cmd("colorscheme cyberdream") -- set the colorscheme
-	-- 	end,
-	-- },
+	-- nightfox colorschemes (have nice dark color schemes)
+	--{ "EdenEast/nightfox.nvim" },
 
 	-- Highlight todo, notes, etc in comments
 	{
