9e3c012d8dc83e1faf466b90995d97c284e2ec5a
Alex Eski <alex.eski@tapcheck.com>
2024-09-12 10:50:36 -0700

    september 2024 changes 4

--- init.lua
+++ init.lua
@@ -339,7 +339,7 @@ require("lazy").setup({
 				pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				tsserver = {},
+				ts_ls = {},
 
 				lua_ls = {
 					-- cmd = {...},
